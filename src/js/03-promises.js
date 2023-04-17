ca4d06609b805e1c6d15d84f5824434f45f75715
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-04-18 00:02:01 +0300

    Solved Task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const btnCreateEl = document.querySelector('button[type="submit"]');
+const inputDelayEl = document.querySelector('input[name="delay"]');
+const inputStepEl = document.querySelector('input[name="step"]');
+const inputAmountEl = document.querySelector('input[name="amount"]');
+
+btnCreateEl.addEventListener('click', onClickMakeArrayP);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          )
+        );
+      } else {
+        reject(
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          )
+        );
+      }
+    }, delay);
+  });
+}
+
+function onClickMakeArrayP(evt) {
+  evt.preventDefault();
+  let delay = parseInt(inputDelayEl.value);
+  const promises = [];
+  for (let i = 1; i <= parseInt(inputAmountEl.value); i += 1) {
+    if (i > 1) {
+      delay += parseInt(inputStepEl.value);
+    }
+
+    promises.push(createPromise(i, delay));
   }
+  return Promise.all(promises)
+    .then(res => res)
+    .catch(res => res);
 }
